03b470424f1bdda771e11471f0e258b8e9c3a84b
Sai <saikumarbt@gmail.com>
2018-01-27 00:20:36 +0530

    Fixed Code issues & upgraded to Bootstrap 4 Beta

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,43 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
 
-// Compile SASS to regularise in Css
-
-gulp.task('sass', function(){
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-    .pipe(sass())
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
-// Move JS files to SRC 
-gulp.task('js', function(){
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/tether/dist/js/tether.min.js'])
-    .pipe(gulp.dest("src/js"))
-    .pipe(browserSync.stream());
+// Move JS Files to src/js
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
 });
 
-// Watch Sass & Serve (Development Servers)
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
 
-gulp.task('serve', ['sass'], function(){
     browserSync.init({
-        server: "./src"
+        server: "./src"  
     });
-    
+
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-// Move Font Awesome to SRC folder
-
-gulp.task('fonts', function(){
-    return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest("src/fonts"));
-});
-
-// Move Font Awesome Css file to SRC Folder
-
-gulp.task('fa', function(){
-    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'));
+// Move Fonts to src/fonts
+gulp.task('fonts', function() {
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('src/fonts'))
 })
 
-gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
+// Move Font Awesome CSS to src/css
+gulp.task('fa', function() {
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest('src/css'))
+})
+
+gulp.task('default', ['js','serve', 'fa', 'fonts']);
